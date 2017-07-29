60ff0fc2e421bde6cf02a2bfb1bcb53123498b93
Aleksander Fal <falalexandr007@gmail.com>
2017-07-29 23:09:31 +0700

    Fixed exception throwing in Matrix. Added unit-tests for Matrix.

--- src/main/data/Matrix.java
+++ src/main/data/Matrix.java
@@ -1,5 +1,9 @@
 package main.data;
 
+import com.sun.istack.internal.NotNull;
+import main.data.exceptions.BadIndexException;
+import main.data.exceptions.MatrixIllegalArgumentException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,31 +19,23 @@ public class Matrix <T extends Number> {
     /**
      * Constructs matrix from {@link ArrayList} of {@link ArrayList<T>}
      * @param matrix {@link ArrayList} from which to construct matrix
+     * @throws MatrixIllegalArgumentException if rows in {@link ArrayList} have different length or {@code matrix == null}
      */
     public Matrix(ArrayList<ArrayList<T>> matrix) {
+        if(matrix == null)
+            throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.ARGUMENT_CANT_BE_NULL_MESSAGE);
+
         int length = matrix.get(0).size();
         for(ArrayList<T> aMatrix : matrix)
             if(aMatrix.size() != length)
-                throw new IllegalArgumentException("Rows can't have different length");
+                throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.DIFFERENT_ROWS_LENGTH_MESSAGE);
 
-        this.matrix = new ArrayList<>(matrix);
-        rows = this.matrix.size();
-        cols = this.matrix.get(0).size();
-    }
-
-    /**
-     * Constructs matrix from two-dimensional array
-     * @param matrix array from which to construct matrix
-     */
-    public Matrix(T[][] matrix) {
-        this.matrix = new ArrayList<>(matrix.length);
-
-        int length = matrix[0].length;
-        for (T[] aMatrix : matrix) {
-            if (aMatrix.length != length)
-                throw new IllegalArgumentException("Rows can't have different length");
-
-            this.matrix.add(new ArrayList<>(Arrays.asList(aMatrix)));
+        this.matrix = new ArrayList<>();
+        int size = matrix.size();
+        for(int i = 0; i < size; i++) {
+            this.matrix.add(new ArrayList<>());
+            ArrayList<T> new_row = this.matrix.get(i);
+            new_row.addAll(matrix.get(i));
         }
 
         rows = this.matrix.size();
@@ -47,29 +43,72 @@ public class Matrix <T extends Number> {
     }
 
     /**
-     * Constructs empty dimensions x dimensions square matrix
-     * @param dimensions length of rows and columns
+     * Constructs matrix from two-dimensional array
+     * @param matrix array from which to construct matrix
+     * @throws MatrixIllegalArgumentException if rows in array have different length or {@code matrix == null}
      */
-    public Matrix(int dimensions) {
-        this.rows = this.cols = dimensions;
-        this.matrix = new ArrayList<>(dimensions);
+    public Matrix(T[][] matrix) {
+        if(matrix == null)
+            throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.ARGUMENT_CANT_BE_NULL_MESSAGE);
 
-        for(int i = 0; i < dimensions; i++)
-            this.matrix.add(new ArrayList<>(dimensions));
+        this.matrix = new ArrayList<>(matrix.length);
+
+        int length = matrix[0].length;
+        for (T[] aMatrix : matrix) {
+            if (aMatrix.length != length)
+                throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.DIFFERENT_ROWS_LENGTH_MESSAGE);
+
+            this.matrix.add(new ArrayList<>(Arrays.asList(aMatrix)));
+        }
+
+        rows = matrix.length;
+        cols = matrix[0].length;
     }
 
     /**
-     * Constructs empty rows x cols matrix
-     * @param rows number of rows
-     * @param cols number of columns
+     * Constructs dimensions x dimensions square matrix filled with zeros
+     * @param dimensions length of rows and columns; must be greater than 2
+     * @throws MatrixIllegalArgumentException if {@code dimensions < 2}
+     */
+    public Matrix(int dimensions) {
+        if(dimensions < 2)
+            throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.WRONG_DIMENSIONS_NUMBER_MESSAGE);
+
+        rows = cols = dimensions;
+        matrix = new ArrayList<>(dimensions);
+
+        for(int i = 0; i < dimensions; i++) {
+            matrix.add(new ArrayList<>(dimensions));
+
+            ArrayList<T> row = matrix.get(i);
+            for(int j = 0; j < dimensions; j++)
+                row.add((T) Integer.valueOf(0));
+        }
+    }
+
+    /**
+     * Constructs rows x cols matrix filled with zeros
+     * @param rows number of rows; must be greater than 2
+     * @param cols number of columns; must be greater than 2
+     * @throws MatrixIllegalArgumentException if {@code rows < 2 || cols < 2}
      */
     public Matrix(int rows, int cols) {
+        if(rows < 2)
+            throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.WRONG_ROWS_NUMBER_MESSAGE);
+        if(cols < 2)
+            throw new MatrixIllegalArgumentException(MatrixIllegalArgumentException.WRONG_COLUMNS_NUMBER_MESSAGE);
+
         this.rows = rows;
         this.cols = cols;
-        this.matrix = new ArrayList<>(rows);
+        matrix = new ArrayList<>(rows);
 
-        for(int i = 0; i < rows; i++)
-            this.matrix.add(new ArrayList<>(cols));
+        for(int i = 0; i < rows; i++) {
+            matrix.add(new ArrayList<>(cols));
+
+            ArrayList<T> row = matrix.get(i);
+            for(int j = 0; j < cols; j++)
+                row.add((T) Integer.valueOf(0));
+        }
     }
 
     /**
@@ -77,14 +116,15 @@ public class Matrix <T extends Number> {
      * @param i row number
      * @param j column number
      * @return element at position [i][j]
+     * @throws BadIndexException if {@code i >= this.rows || i < 0 || j >= this.cols || j < 0}
      */
     public T get(int i, int j) {
         if(i >= rows || j >= cols)
-            throw new IndexOutOfBoundsException();
+            throw new BadIndexException(BadIndexException.INDEX_OUT_OF_BOUNDS_MESSAGE);
         if(i < 0 || j < 0)
-            throw new IndexOutOfBoundsException();
+            throw new BadIndexException(BadIndexException.NEGATIVE_INDEX_MESSAGE);
 
-        return this.matrix.get(i).get(j);
+        return matrix.get(i).get(j);
     }
 
     /**
@@ -92,14 +132,15 @@ public class Matrix <T extends Number> {
      * @param i row number
      * @param j column number
      * @param val value to set
+     * @throws BadIndexException if {@code i >= this.rows || i < 0 || j >= this.cols || j < 0}
      */
     public void set(int i, int j, T val) {
         if(i >= rows || j >= cols)
-            throw new IndexOutOfBoundsException();
+            throw new BadIndexException(BadIndexException.INDEX_OUT_OF_BOUNDS_MESSAGE);
         if(i < 0 || j < 0)
-            throw new IndexOutOfBoundsException();
+            throw new BadIndexException(BadIndexException.NEGATIVE_INDEX_MESSAGE);
 
-        this.matrix.get(i).set(j, val);
+        matrix.get(i).set(j, val);
     }
 
     /**
---
+++ src/test/MatrixTest.java
@@ -0,0 +1,306 @@
+package test;
+
+import main.data.Matrix;
+import main.data.exceptions.BadIndexException;
+import main.data.exceptions.MatrixIllegalArgumentException;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Feature: testing methods from Matrix class
+ * @author Alexander Fal (falalexandr007@gmail.com)
+ */
+class MatrixTest {
+    private static Matrix<Integer> matrix;
+    private static final int dimensions = 5;
+    @BeforeAll
+    static void beforeAll() {
+        matrix = new Matrix<>(dimensions);
+    }
+
+    /**
+     * Feature: testing Matrix object creation
+     * @author Alexander Fal (falalexandr007@gmail.com)
+     */
+    @Nested
+    @DisplayName("Constructors")
+    class ConstructorsTests {
+
+        /**
+         * Create matrix from {@link ArrayList}
+         * 1. Create {@link ArrayList}
+         * 2. Create {@link Matrix} from {@link ArrayList}
+         *  a. check if each element in {@link Matrix} equals to corresponding element in {@link ArrayList}
+         * @throws MatrixIllegalArgumentException if rows in {@link ArrayList} have different length or {@code array == null}
+         */
+        @Test
+        void testCreateMatrixFromArrayList() {
+            ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+
+            int rows = 3, cols = 4;
+            for(int i = 0; i < rows; i++) {
+                arr.add(new ArrayList<>());
+                ArrayList<Integer> row = arr.get(i);
+                for(int j = 0; j < cols; j++)
+                    row.add(j);
+            }
+
+            Matrix<Integer> matrix = new Matrix<>(arr);
+            for(int i = 0; i < rows; i++) {
+                ArrayList<Integer> row = arr.get(i);
+                for (int j = 0; j < cols; j++)
+                    assertEquals(matrix.get(i, j), row.get(j));
+            }
+        }
+
+        /**
+         * Create matrix from {@link ArrayList}
+         * 1. Create {@link ArrayList} reference and assign null
+         * 2. Create {@link Matrix} from {@link ArrayList}
+         *  a. check if exception with correct message was thrown
+         * @throws MatrixIllegalArgumentException if rows in {@link ArrayList} have different length or {@code array == null}
+         */
+        @Test
+        void testCreateMatrixFromArrayListNull() {
+            ArrayList<ArrayList<Integer>> arr = null;
+
+            MatrixIllegalArgumentException exception = assertThrows(MatrixIllegalArgumentException.class, () -> new Matrix<>(arr));
+            assertEquals(exception.getMessage(), MatrixIllegalArgumentException.ARGUMENT_CANT_BE_NULL_MESSAGE);
+        }
+
+        /**
+         * Create matrix from two-dimensional array
+         * 1. Create {@link ArrayList}
+         * 2. Create {@link Matrix} from {@link ArrayList}
+         *  a. check if each element in {@link Matrix} equals to corresponding element in {@link ArrayList}
+         * @throws MatrixIllegalArgumentException if rows in {@link ArrayList} have different length or {@code array == null}
+         */
+        @Test
+        void testCreateMatrixFromArray() {
+            int rows = 4, cols = 3;
+            Integer[][] arr = new Integer[rows][cols];
+            for(int i = 0; i < rows; i++)
+                for(int j = 0; j < cols; j++)
+                    arr[i][j] = i;
+
+            Matrix<Integer> matrix = new Matrix<> (arr);
+            for(int i = 0; i < rows; i++)
+                for(int j = 0; j < cols; j++)
+                    assertEquals(matrix.get(i,j), arr[i][j]);
+        }
+
+        /**
+         * Create matrix from null-reference to two-dimensional array
+         * 1. Create array reference and assign null
+         * 2. Create {@link Matrix} from array
+         *  a. check if exception with correct message was thrown
+         * @throws MatrixIllegalArgumentException if rows in array have different length or {@code array == null}
+         */
+        @Test
+        void testCreateMatrixFromArrayNull() {
+            Integer[][] arr = null;
+
+            MatrixIllegalArgumentException exception = assertThrows(MatrixIllegalArgumentException.class, () -> new Matrix<>(arr));
+            assertEquals(exception.getMessage(), MatrixIllegalArgumentException.ARGUMENT_CANT_BE_NULL_MESSAGE);
+        }
+
+        /**
+         * Create square matrix filled with zeros
+         * 1. Create matrix with valid dimensions argument
+         * @throws MatrixIllegalArgumentException if {@code dimensions < 2}
+         */
+        @Test
+        void testCreateZeroSquareMatrix() {
+            new Matrix<Integer>(2);
+        }
+
+        /**
+         * Create square matrix with wrong dimensions
+         * 1. Create matrix with {@code dimensions < 2}
+         *  a. check that exception with correct error message was thrown
+         * @throws MatrixIllegalArgumentException if {@code dimensions < 2}
+         */
+        @Test
+        void testCreateZeroSquareMatrixBadDimensions() {
+            Exception exception = assertThrows(MatrixIllegalArgumentException.class, () -> new Matrix<Integer>(1));
+            assertEquals(exception.getMessage(), MatrixIllegalArgumentException.WRONG_DIMENSIONS_NUMBER_MESSAGE);
+        }
+
+        /**
+         * Create matrix filled with zeros
+         * 1. Create matrix with valid rows and cols arguments
+         * @throws MatrixIllegalArgumentException if {@code rows < 2 || cols < 2}
+         */
+        @Test
+        void testCreateZeroMatrix() {
+            new Matrix<Integer>(2,2);
+        }
+
+        /**
+         * Create matrix with wrong cols argument
+         * 1. Create matrix with {@code cols < 2}
+         *  a. check that exception with correct error message was thrown
+         * @throws MatrixIllegalArgumentException if {@code cols < 2 || rows < 2}
+         */
+        @Test
+        void testCreateZeroMatrixWrongColumns() {
+            Exception exception = assertThrows(MatrixIllegalArgumentException.class, () -> new Matrix<Integer>(2, 1));
+            assertEquals(exception.getMessage(), MatrixIllegalArgumentException.WRONG_COLUMNS_NUMBER_MESSAGE);
+        }
+
+        /**
+         * Create matrix with wrong rows argument
+         * 1. Create matrix with {@code rows < 2}
+         *  a. check that exception with correct error message was thrown
+         * @throws MatrixIllegalArgumentException if {@code cols < 2 || rows < 2}
+         */
+        @Test
+        void testCreateZeroMatrixWrongRows() {
+            Exception exception = assertThrows(MatrixIllegalArgumentException.class, () -> new Matrix<Integer>(1,2));
+            assertEquals(exception.getMessage(), MatrixIllegalArgumentException.WRONG_ROWS_NUMBER_MESSAGE);
+        }
+    }
+
+    /**
+     * Feature: testing mutator methods from Matrix class
+     * @author Alexander Fal (falalexandr007@gmail.com)
+     */
+    @Nested
+    @DisplayName("Mutators")
+    class MutatorsTests {
+        /**
+         * Valid arguments
+         * 1. Use {@link Matrix#set(int, int, Number)} method with valid arguments
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testSetValidIndex() {
+            matrix.set(0, 0, 21);
+        }
+
+        /**
+         * Row index is negative
+         * 1. Use {@link Matrix#set(int, int, Number)} method with negative row index
+         *  a. Check if exception with correct message was thrown
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testSetNegativeRowIndex() {
+            BadIndexException exception = assertThrows(BadIndexException.class, () -> matrix.set(-1, 0, 21));
+            assertEquals(exception.getMessage(), BadIndexException.NEGATIVE_INDEX_MESSAGE);
+        }
+
+        /**
+         * Column index is negative
+         * 1. Use {@link Matrix#set(int, int, Number)} method with negative column index
+         *  a. Check if exception with correct message was thrown
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testSetNegativeColumnIndex() {
+            BadIndexException exception = assertThrows(BadIndexException.class, () -> matrix.set(0, -1, 21));
+            assertEquals(exception.getMessage(), BadIndexException.NEGATIVE_INDEX_MESSAGE);
+        }
+
+        /**
+         * Row index is >= {@link Matrix#getRows()}
+         * 1. Use {@link Matrix#set(int, int, Number)} method with row >= {@link Matrix#getRows()}
+         *  a. Check if exception with correct message was thrown
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testSetOutOfBoundsRowIndex() {
+            BadIndexException exception = assertThrows(BadIndexException.class, () -> matrix.set(21, 0, 21));
+            assertEquals(exception.getMessage(), BadIndexException.INDEX_OUT_OF_BOUNDS_MESSAGE);
+        }
+
+        /**
+         * Column index is >= {@link Matrix#getCols()}
+         * 1. Use {@link Matrix#set(int, int, Number)} method with col >= {@link Matrix#getCols()}
+         *  a. Check if exception with correct message was thrown
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testSetOutOfBoundsColumnIndex() {
+            BadIndexException exception = assertThrows(BadIndexException.class, () -> matrix.set(0, 21, 21));
+            assertEquals(exception.getMessage(), BadIndexException.INDEX_OUT_OF_BOUNDS_MESSAGE);
+        }
+    }
+
+    /**
+     * Feature: testing accessor methods from Matrix class
+     * @author Alexander Fal (falalexandr007@gmail.com)
+     */
+    @Nested
+    @DisplayName("Accessors")
+    class AccessorsTests {
+        /**
+         * Get element with valid index
+         * 1. Use {@link Matrix#get(int, int)} with valid arguments
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testGetValidIndex() {
+            matrix.get(0,0);
+        }
+
+        /**
+         * Get element with negative index
+         * 1. Use {@link Matrix#get(int, int)} with negative index
+         *  a. check if exception with correct message was thrown
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testGetNegativeIndex() {
+            BadIndexException exception = assertThrows(BadIndexException.class, () -> matrix.get(-1, 0));
+            assertEquals(exception.getMessage(), BadIndexException.NEGATIVE_INDEX_MESSAGE);
+        }
+
+        /**
+         * Get element with index >= {@link Matrix#getRows()}
+         * 1. Use {@link Matrix#get(int, int)} with index >= {@link Matrix#getRows()}
+         *  a. check if exception with correct message was thrown
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testGetOutOfBoundsIndex() {
+            BadIndexException exception = assertThrows(BadIndexException.class, () -> matrix.get(21, 0));
+            assertEquals(exception.getMessage(), BadIndexException.INDEX_OUT_OF_BOUNDS_MESSAGE);
+        }
+
+        /**
+         * Get number of rows
+         * 1. Use {@link Matrix#getRows()}
+         *  a. check if returned value equals to value given to constructor
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testGetRows() {
+            assertEquals(matrix.getRows(), dimensions);
+        }
+
+        /**
+         * Get number of columns
+         * 1. Use {@link Matrix#getCols()}
+         *  a. check if returned value equals to value given to constructor
+         * @throws BadIndexException if row or column index is negative
+         *                           or row >= {@link Matrix#getRows()} or column >= {@link Matrix#getCols()}
+         */
+        @Test
+        void testGetCols() {
+            assertEquals(matrix.getCols(), dimensions);
+        }
+    }
+}
