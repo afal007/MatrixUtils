f1f7dc29a72b04a6b0a660945993fa81ff2d407d
Aleksander Fal <falalexandr007@gmail.com>
2017-07-29 17:56:52 +0700

    Added JavaDoc comments. Moved Matrix.java to package "main".

--- src/main/Matrix.java
+++ src/main/Matrix.java
@@ -1,13 +1,21 @@
+package main;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * author: Alexander Fal (falalexandr007@gmail.com)
+ * Represents basic matrix structure
+ * @author Alexander Fal (falalexandr007@gmail.com)
+ * @version 1.0
  */
 public class Matrix <T extends Number> {
     private ArrayList<ArrayList<T>> matrix;
     private int rows, cols;
 
+    /**
+     * Constructs matrix from {@link ArrayList} of {@link ArrayList<T>}
+     * @param matrix {@link ArrayList} from which to construct matrix
+     */
     public Matrix(ArrayList<ArrayList<T>> matrix) {
         int length = matrix.get(0).size();
         for(ArrayList<T> aMatrix : matrix)
@@ -19,6 +27,10 @@ public class Matrix <T extends Number> {
         cols = this.matrix.get(0).size();
     }
 
+    /**
+     * Constructs matrix from two-dimensional array
+     * @param matrix array from which to construct matrix
+     */
     public Matrix(T[][] matrix) {
         this.matrix = new ArrayList<>(matrix.length);
 
@@ -34,6 +46,10 @@ public class Matrix <T extends Number> {
         cols = this.matrix.get(0).size();
     }
 
+    /**
+     * Constructs empty dimensions x dimensions square matrix
+     * @param dimensions length of rows and columns
+     */
     public Matrix(int dimensions) {
         this.rows = this.cols = dimensions;
         this.matrix = new ArrayList<>(dimensions);
@@ -42,6 +58,11 @@ public class Matrix <T extends Number> {
             this.matrix.add(new ArrayList<>(dimensions));
     }
 
+    /**
+     * Constructs empty rows x cols matrix
+     * @param rows number of rows
+     * @param cols number of columns
+     */
     public Matrix(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
@@ -51,6 +72,12 @@ public class Matrix <T extends Number> {
             this.matrix.add(new ArrayList<>(cols));
     }
 
+    /**
+     * Returns element at i row, j column ( matrix[i][j] )
+     * @param i row number
+     * @param j column number
+     * @return element at position [i][j]
+     */
     public T get(int i, int j) {
         if(i >= rows || j >= cols)
             throw new IndexOutOfBoundsException();
@@ -60,6 +87,12 @@ public class Matrix <T extends Number> {
         return this.matrix.get(i).get(j);
     }
 
+    /**
+     * Sets element at i row, j column ( matrix[i][j] ) to val
+     * @param i row number
+     * @param j column number
+     * @param val value to set
+     */
     public void set(int i, int j, T val) {
         if(i >= rows || j >= cols)
             throw new IndexOutOfBoundsException();
@@ -69,10 +102,18 @@ public class Matrix <T extends Number> {
         this.matrix.get(i).set(j, val);
     }
 
+    /**
+     * Returns number of rows in matrix
+     * @return number of rows in matrix
+     */
     public int getRows() {
         return rows;
     }
 
+    /**
+     * Returns number of columns in matrix
+     * @return number of columns in matrix
+     */
     public int getCols() {
         return cols;
     }
